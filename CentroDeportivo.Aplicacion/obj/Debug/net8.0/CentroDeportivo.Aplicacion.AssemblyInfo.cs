df6294b7435b6454c9e269611f787d5f792ceb80
Adrián A. Sambido <113736411+AdrianASambido@users.noreply.github.com>
2026-05-01 19:47:13 -0300

    primer ctualización

--- CentroDeportivo.Aplicacion/obj/Debug/net8.0/CentroDeportivo.Aplicacion.AssemblyInfo.cs
+++ CentroDeportivo.Aplicacion/obj/Debug/net8.0/CentroDeportivo.Aplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroDeportivo.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b33871d9072d6f00e3895297ce8cad4a8851d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroDeportivo.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroDeportivo.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
