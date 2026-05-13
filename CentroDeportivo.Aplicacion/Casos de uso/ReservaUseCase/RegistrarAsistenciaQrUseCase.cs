7a6eef3a01a7478654f9178ab70b2a51b126313d
Bautista <bautistaguilarr@gmail.com>
2026-05-13 01:55:46 -0300

    .

--- CentroDeportivo.Aplicacion/Casos de uso/ReservaUseCase/RegistrarAsistenciaQrUseCase.cs
+++ CentroDeportivo.Aplicacion/Casos de uso/ReservaUseCase/RegistrarAsistenciaQrUseCase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CentroDeportivo.Aplicacion.Interfaces;
+using CentroDeportivo.Aplicacion.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace CentroDeportivo.Aplicacion.Casos_de_uso.ReservaUseCase
 {
-    public class RegistrarAsistenciaQrUseCase
+    public class RegistrarAsistenciaQrUseCase(IReservaRepositorio repoReserva)
     {
+        public async Task Ejecutar(string tokenEscaneado)
+        {
+
+            var reserva = await repoReserva.ObtenerPorQrTokenAsync(tokenEscaneado);
+
+            if (reserva == null)
+            {
+                throw new Exception("Error: Código QR inválido o inexistente.");
+            }
+
+
+            if (reserva.Asistencia == Asistencia.Presente)
+            {
+                throw new Exception("Error: Esta asistencia ya fue registrada anteriormente.");
+            }
+
+
+            if (reserva.Turno.Fecha != DateOnly.FromDateTime(DateTime.Now))
+            {
+                throw new Exception("Error: El QR corresponde a una reserva de otro día");
+            }
+
+
+            reserva.Asistencia = Asistencia.Presente;
+            await repoReserva.ActualizarAsync(reserva);
+        }
     }
 }
