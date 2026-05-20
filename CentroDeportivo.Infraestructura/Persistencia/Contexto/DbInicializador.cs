87ee392a10da59e6b67e9b9722e496d2729da632
Nelson Joaquin Pinanelli <nelsonjp1999@gmail.com>
2026-05-20 15:33:50 -0300

    .

--- CentroDeportivo.Infraestructura/Persistencia/Contexto/DbInicializador.cs
+++ CentroDeportivo.Infraestructura/Persistencia/Contexto/DbInicializador.cs
@@ -26,27 +26,7 @@ public static class DbInicializador
             context.SaveChanges();
         }
 
-        // SeedCatalogoReservas(context);
+        
     }
-    /*
-    /// <summary>Cancha, profesor y actividades base si aún no existen (útil tras primera ejecución sólo con admin).</summary>
-    private static void SeedCatalogoReservas(CentroDeportivoContext context)
-    {
-        if (!context.Canchas.Any())
-            context.Canchas.Add(new Cancha(1, 30));
-
-        if (!context.Profesores.Any())
-            context.Profesores.Add(new Profesor("Profe", "General", "00000000"));
-
-        if (!context.Actividades.Any())
-        {
-            context.Actividades.Add(new Actividad("Pádel", "Clase grupal", 1500));
-            context.Actividades.Add(new Actividad("Fútbol", "Clase grupal", 1200));
-            context.Actividades.Add(new Actividad("Vóley", "Clase grupal", 1100));
-            context.Actividades.Add(new Actividad("Básquet", "Clase grupal", 1100));
-        }
-
-        context.SaveChanges();
-    }
-    */
+   
 }
